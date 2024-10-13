cd40bd13d45aed45369317e9aa5000407c7ee95d
okm321 <okmkm321@gmail.com>
2024-10-13 20:05:08 +0900

    fix: brew

--- brew-init.sh
+++ brew-init.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
 # homebrewがインストールされていない場合はインストール
-printf "\n\e[35m💥 Start setup Homebrew \e[m\n"
-if ! type brew >/dev/null 2>&1; then
-  printf "\e[32mHomebrew Status: Install Homebrew \e[m\n"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# printf "\n\e[35m💥 Start setup Homebrew \e[m\n"
+# if ! type brew >/dev/null 2>&1; then
+#   printf "\e[32mHomebrew Status: Install Homebrew \e[m\n"
+#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# else
+#   printf "\e[33mHomebrew Status: Already installed Homebrew \e[m\n"
+# fi
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ "$(uname -m)" = "arm64" ]; then
+  (
+    echo
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+  ) >>/Users/${USER}/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
+# Install xcode
+# Check if command line tools are installed
+if ! xcode-select --print-path &>/dev/null; then
+  # Install command line tools
+  echo "Command line tools not found. Installing..."
+  xcode-select --install
 else
-  printf "\e[33mHomebrew Status: Already installed Homebrew \e[m\n"
+  echo "Command line tools are already installed."
 fi
 
 brew bundle --global
