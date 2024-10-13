d352b2d103c74aaea4467c9b13a7be23ad910b68
okm321 <okmkm321@gmail.com>
2024-10-13 21:38:42 +0900

    fix: brew install

--- brew-init.sh
+++ brew-init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # homebrewがインストールされていない場合はインストール
-# printf "\n\e[35m💥 Start setup Homebrew \e[m\n"
 # if ! type brew >/dev/null 2>&1; then
 #   printf "\e[32mHomebrew Status: Install Homebrew \e[m\n"
 #   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -10,15 +9,17 @@
 # fi
 
 # Install brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-if [ "$(uname -m)" = "arm64" ]; then
-  (
+if ! type brew >/dev/null 2>&1; then
+  printf "\n\e[35m💥 Start setup Homebrew \e[m\n"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if [ "$(uname -m)" = "arm64" ]; then
+    (
     echo
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-  ) >>/Users/${USER}/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+    ) >>/Users/${USER}/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
 fi
-
 # Install xcode
 # Check if command line tools are installed
 if ! xcode-select --print-path &>/dev/null; then
